3dab288eaf412a081ede78358a0c41e8b9d615c5
somya <somya.singhal0797@gmail.com>
2019-02-10 13:44:24 +0530

    initialize

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+EcomApi::Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json },path: '/' do
+    scope module: :v1 do
+      # We are going to list our resources here
+    end
+  end  
+
+end
