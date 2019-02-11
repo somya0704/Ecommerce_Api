cc97f170b5091b3825bd2859b42ca88d04e6abd4
somya <somya.singhal0797@gmail.com>
2019-02-11 09:50:52 +0530

    rspec error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
     protect_from_forgery with: :null_session
-
+    #before_action :authenticate_user!
     include Authenticable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
+require 'api_constraints'
 EcomApi::Application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json },path: '/' do
-    scope module: :v1 do
-    
-      resources :users, :only => [:show, :create, :update, :destroy] do
-        # this is the line
-        
-      end
-      resources :sessions, :only => [:create, :destroy]
-      resources :products, :only => [:create, :update, :destroy]
-      resources :orders, :only => [:index, :show]
-    end
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+        resources :users, only: [:show, :create, :update, :destroy] do
+          resources :products, only: [:create,:update, :destroy]
+          resources :orders, only: [:index, :show, :create]
+        end
+        resources :sessions, only: [:create, :destroy]
+        resources :products, only: [:show, :index]
+      end 
   end  
-
 end
+
